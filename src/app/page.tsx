1fb1aba192bbc1921ebfc42aaab6df646ff297cc
Richard Mezzomo <richardmezzomo1@gmail.com>
2024-01-30 15:43:48 -0300

    add configs tailwind

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,12 +4,12 @@ import logo from '../assets/logo.svg'
 
 export default function Home() {
   return (
-    <main className="w-screen h-screen flex flex-col justify-center items-center bg-[#F8F4F0]">
+    <main className="flex h-screen w-screen flex-col items-center justify-center bg-white">
       <header>
         <Image src={logo} alt="logo" width={100} height={100} />
       </header>
-      <h1 className="font-sans text-2xl pt-5 text-neutral-800">Hello World!</h1>
-      <p className="font-mono text-xl text-neutral-800">
+      <h1 className="pt-5 font-sans text-2xl text-black">Hello World!</h1>
+      <p className="font-mono text-xl text-black">
         Em Construção: Aguarde a Versão 2! 🚧
       </p>
     </main>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,11 @@ const config: Config = {
         sans: ['var(--font-montserrat)'],
         mono: ['var(--font-source-code-pro)'],
       },
+      colors: {
+        white: '#F8F4F0',
+        black: '#222222',
+        gray: 'rgba(34, 34, 34, 0.40)',
+      },
     },
   },
   plugins: [],
