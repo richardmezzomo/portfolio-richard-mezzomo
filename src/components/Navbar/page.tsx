be8cf866c38b37cbf38ae911f1b85d17254a448d
Richard Mezzomo <richardmezzomo1@gmail.com>
2024-01-31 14:40:47 -0300

    feat: add Navbar Component

--- src/components/Navbar/page.tsx
+++ src/components/Navbar/page.tsx
@@ -11,7 +11,7 @@ export default function Navbar() {
 
   return (
     <>
-      <nav className="fixed w-screen bg-gray  px-6 py-4 lg:flex lg:items-center lg:justify-between lg:px-20 lg:py-6">
+      <nav className="fixed z-10 w-screen bg-gray  px-6 py-4 lg:flex lg:items-center lg:justify-between lg:px-20 lg:py-6">
         <div className="">
           <div className="flex items-center justify-between">
             <span>
