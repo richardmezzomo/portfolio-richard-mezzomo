6923de0b4485b8dca08cdf6e4be9227e549b1289
Richard Mezzomo <richardmezzomo1@gmail.com>
2024-01-31 14:29:30 -0300

    feat: add Navbar Component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,9 @@
-import Image from 'next/image'
-
-import logo from '../assets/logo.svg'
+import Navbar from '@/components/Navbar/page'
 
 export default function Home() {
   return (
-    <main className="flex h-screen w-screen flex-col items-center justify-center bg-white">
-      <header>
-        <Image src={logo} alt="logo" width={100} height={100} />
-      </header>
-      <h1 className="pt-5 font-sans text-2xl text-black">Hello World!</h1>
-      <p className="font-mono text-xl text-black">
-        Em Construção: Aguarde a Versão 2! 🚧
-      </p>
-    </main>
+    <>
+      <Navbar />
+    </>
   )
 }
