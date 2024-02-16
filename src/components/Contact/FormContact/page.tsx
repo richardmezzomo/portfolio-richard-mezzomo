0e20883ea905c96ff7c798fdf8bab86b6cf481b5
Richard Mezzomo <richardmezzomo1@gmail.com>
2024-02-16 17:42:51 -0300

    feat: add header desktop form

--- src/components/Contact/FormContact/page.tsx
+++ src/components/Contact/FormContact/page.tsx
@@ -1,6 +1,6 @@
 export default function FormContact() {
   return (
-    <form className="flex flex-col items-center border border-black">
+    <form className="flex flex-col items-center justify-center gap-10 w-full">
       <input type="text" placeholder="Nome" />
       <input type="email" placeholder="E-mail" />
       <input type="text" placeholder="Assunto" />
--- src/components/Contact/page.tsx
+++ src/components/Contact/page.tsx
@@ -1,18 +1,18 @@
-import FormContact from './Form/page'
+import FormContact from './FormContact/page'
 import Image from 'next/image'
 import rocket from '@/assets/icons/rocket.svg'
 
 export default function Contact() {
   return (
     <section className="flex flex-col items-center p-6 lg:px-80">
-      <header className="lg:border-emerald-30'0 lg:flex lg:flex-col lg:border">
-        <span className="flex gap-3 font-mono text-sm">
+      <header className="lg:flex lg:flex-col">
+        <span className="flex gap-3 font-mono text-sm lg:text-lg">
           estamos prontos <Image src={rocket} alt="" />
         </span>
-        <h1 className="pb-3 pt-1 font-sans text-xl font-bold">
+        <h1 className="pb-3 pt-1 font-sans text-xl font-bold lg:text-[40px]">
           Podemos falar de negócios?
         </h1>
-        <p className="text-sm">
+        <p className="text-sm lg:text-lg">
           Estamos prontos para transformar sua visão em realidade digital.
         </p>
       </header>
