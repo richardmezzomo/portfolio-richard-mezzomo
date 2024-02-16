c24863b9b749c668f82e662443fe1af29447d3ef
Richard Mezzomo <richardmezzomo1@gmail.com>
2024-02-16 17:50:44 -0300

    refactor: eslint error

--- src/components/Contact/FormContact/page.tsx
+++ src/components/Contact/FormContact/page.tsx
@@ -1,6 +1,6 @@
 export default function FormContact() {
   return (
-    <form className="flex flex-col items-center justify-center gap-10 w-full">
+    <form className="flex w-full flex-col items-center justify-center gap-10">
       <input type="text" placeholder="Nome" />
       <input type="email" placeholder="E-mail" />
       <input type="text" placeholder="Assunto" />
